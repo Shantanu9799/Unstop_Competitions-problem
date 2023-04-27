cc01fc88a546d0cb249edb7f17a87c27913e08bc
Shantanu9799 <cshantanu712@gmail.com>
2023-04-27 21:50:24 +0530

    day1

---
+++ ToLowerCase.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+        System.out.print(str.toLowerCase());
+    }
+}
